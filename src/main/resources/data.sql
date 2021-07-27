f247b71405b0de3938837d847d98082c3a47b37f
UmeshGunasekara <umesh.gunasekara.n@gmail.com>
2021-07-27 10:03:13 +0800

    MORALEARNLENOVO - master - update - SLMORA - UTC202107270203

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,15 @@
 /* CREATE TABLE */
-CREATE TABLE sgg_country
-(
-    country_id      DOUBLE,
-    country_name    VARCHAR(100),
-    country_alpha_2 VARCHAR(100),
-    country_alpha_3 VARCHAR(100),
-    country_numeric DOUBLE
+CREATE TABLE sgg_country (
+    country_id INT NOT NULL AUTO_INCREMENT,
+    country_name VARCHAR(100) NOT NULL UNIQUE,
+    country_alpha_2 CHAR(2) NOT NULL UNIQUE,
+    country_alpha_3 CHAR(3) NOT NULL UNIQUE,
+    country_numeric INT NOT NULL UNIQUE,
+    PRIMARY KEY  (country_id)
 );
+
+CREATE INDEX ix_country_a_country_alpha_3 ON sgg_country (country_alpha_3)
+;
 
 /* INSERT QUERY NO: 1 */
 INSERT INTO sgg_country(country_id, country_name, country_alpha_2, country_alpha_3, country_numeric)
@@ -478,36 +481,23 @@ VALUES (117, 'Kiribati', 'KI', 'KIR', 296);
 
 /* INSERT QUERY NO: 118 */
 INSERT INTO sgg_country(country_id, country_name, country_alpha_2, country_alpha_3, country_numeric)
-VALUES (118, 'Korea (the Democratic People\'s Republic of)', 'KP', 'PRK', 408
-);
+VALUES (118, 'Korea (the Democratic Peoples Republic of)', 'KP', 'PRK', 408);
 
 /* INSERT QUERY NO: 119 */
 INSERT INTO sgg_country(country_id, country_name, country_alpha_2, country_alpha_3, country_numeric)
-VALUES
-(
-    119, 'Korea (the Republic of)', 'KR', 'KOR', 410
-);
+VALUES (119, 'Korea (the Republic of)', 'KR', 'KOR', 410);
 
 /* INSERT QUERY NO: 120 */
 INSERT INTO sgg_country(country_id, country_name, country_alpha_2, country_alpha_3, country_numeric)
-VALUES
-(
-    120, 'Kuwait', 'KW', 'KWT', 414
-);
+VALUES (120, 'Kuwait', 'KW', 'KWT', 414);
 
 /* INSERT QUERY NO: 121 */
 INSERT INTO sgg_country(country_id, country_name, country_alpha_2, country_alpha_3, country_numeric)
-VALUES
-(
-    121, 'Kyrgyzstan', 'KG', 'KGZ', 417
-);
+VALUES (121, 'Kyrgyzstan', 'KG', 'KGZ', 417);
 
 /* INSERT QUERY NO: 122 */
 INSERT INTO sgg_country(country_id, country_name, country_alpha_2, country_alpha_3, country_numeric)
-VALUES
-(
-    122, 'Lao People\'s Democratic Republic (the)', 'LA', 'LAO', 418
-);
+VALUES (122, 'Lao People Democratic Republic (the)', 'LA', 'LAO', 418);
 
 /* INSERT QUERY NO: 123 */
 INSERT INTO sgg_country(country_id, country_name, country_alpha_2, country_alpha_3, country_numeric)
@@ -1025,12 +1015,16 @@ VALUES (0, '', '', '', 0);
 /* Showing results for All country GDP Dataset.xlsx */
 
 /* CREATE TABLE */
-CREATE TABLE sgg_gdp_growth
-(
-    gdp_growth_country_alpha_3 VARCHAR(100),
-    gdp_growth_year            DOUBLE,
-    gdp_growth_value           DOUBLE
+CREATE TABLE sgg_gdp_growth (
+    gdp_growth_id INT NOT NULL AUTO_INCREMENT,
+    gdp_growth_year INT NOT NULL UNIQUE,
+    gdp_growth_value DECIMAL(12, 1) NOT NULL UNIQUE,
+    gdp_growth_country_alpha_3 CHAR(3) NOT NULL UNIQUE,
+    PRIMARY KEY  (gdp_growth_id)
 );
+
+CREATE INDEX ix_gdp_growth_a_gdp_growth_country_alpha_3 ON sgg_gdp_growth (gdp_growth_country_alpha_3)
+;
 
 /* INSERT QUERY NO: 1 */
 INSERT INTO sgg_gdp_growth(gdp_growth_country_alpha_3, gdp_growth_year, gdp_growth_value)
